3bbfcbfb11d9f387566f34bf375f3c033c519f83
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-09-28 14:24:06 -0700

    Add the Race Condition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import "@finos/perspective-viewer/dist/css/themes.css";
 import type { HTMLPerspectiveViewerElement } from "@finos/perspective-viewer";
+import "@finos/perspective-viewer/dist/css/themes.css";
+import React from "react";
 
 export default function Home() {
-    const ref = React.useRef<HTMLPerspectiveViewerElement>(null);
-    React.useEffect(() => {
-        Promise.all([
-            import("@finos/perspective-viewer"),
-            import("@finos/perspective-viewer-datagrid"),
-            import("@finos/perspective-viewer-d3fc"),
-            import("@finos/perspective"),
+  const ref = React.useRef<HTMLPerspectiveViewerElement>(null);
+  React.useEffect(() => {
+    Promise.all([
+      import("@finos/perspective-viewer"),
+      import("@finos/perspective-viewer-datagrid"),
+      import("@finos/perspective-viewer-d3fc"),
+      import("@finos/perspective"),
 
-            // @ts-ignore
-            import("superstore-arrow/superstore.arrow"),
-        ]).then(([_, __, ___, perspective, arr]) => {
-            const worker = perspective.default.shared_worker();
-            const table = worker.table(arr.default.slice());
-            ref.current!.load(table);
-        })
-    }, [ref]);
+      // @ts-ignore
+      import("superstore-arrow/superstore.arrow"),
+    ]).then(async ([_, __, ___, perspective, arr]) => {
+      const worker = perspective.default.shared_worker();
+      const table = worker.table(arr.default.slice());
+      await ref.current!.load(table);
+      const config = {
+        plugin: "Treemap",
+      };
+      ref.current!.restore(config);
+    });
+  }, [ref]);
 
-    return (<perspective-viewer ref={ref}></perspective-viewer>);
+  return <perspective-viewer ref={ref}></perspective-viewer>;
 }
